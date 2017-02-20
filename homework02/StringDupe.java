4604789dc4006ffacfb9a86e6bf4cbb936300ec7
annapasano <anna.pasano@gmail.com>
2017-02-20 01:08:39 -0800

    StringDupe
    Forgot to commit this on time

---
+++ homework02/StringDupe.java
@@ -0,0 +1,179 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  StringStuff.java
+ *  Purpose       :  A file full of stuff to do with the Java String class
+ *  Author        :  Anna Pasano
+ *  Date          :  2017-02-06
+ *  Description   :  This file presents a bunch of String-style helper methods.  Although pretty much
+ *                   any and every thing you'd want to do with Strings is already made for you in the
+ *                   Jave String class, this exercise gives you a chance to do it yourself [DIY] for some
+ *                   of it and get some experience with designing code that you can then check out using
+ *                   the real Java String methods [if you want]
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ----------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-19  B.J. Johnson  Initial writing and release
+ *  @version 1.1.0  2017-02-06  Anna Pasano  Fill in methods to make the program actually work
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+public class StringStuff {
+
+  /**
+   * Method to determine if a string contains one of the vowels: A, E, I, O, U, and sometimes Y.
+   * Both lower and upper case letters are handled.  In this case, the normal English rule for Y means
+   * it gets included.
+   *
+   * @param s String containing the data to be checked for &quot;vowel-ness&quot;
+   * @return  boolean which is true if there is a vowel, or false otherwise
+   */
+   public static boolean containsVowel( String s ) {
+      String str = s.toUpperCase();
+      for (int i = 0; i <= str.length() - 1; i++) {
+         char temp = str.charAt(i);
+         if (temp == 'A' || temp == 'E' || temp == 'I' || temp == 'O' || temp == 'U') {
+            return true;
+         }
+      }
+      return false;
+   }
+
+  /**
+   * Method to determine if a string is a palindrome.  Does it the brute-force way, checking
+   * the first and last, second and last-but-one, etc. against each other.  If something doesn't
+   * match that way, returns false, otherwise returns true.
+   *
+   * @param s String containing the data to be checked for &quot;palindrome-ness&quot;
+   * @return  boolean which is true if this a palindrome, or false otherwise
+   */
+   public static boolean isPalindrome( String s ) {
+      String str = s.toUpperCase ();
+      for (int i = 0; i <= str.length() /2; i++) {
+         char firstChar = str.charAt (i);
+         char lastChar = str.charAt(str.length() - 1 - i);
+         if (firstChar != lastChar) {
+            return false;            
+         }
+      }
+      return true;
+   }
+
+  /**
+   * Method to return the characters in a string that correspond to the &quot;EVEN&quot; index
+   * numbers of the alphabet.  The letters B, D, F, H, J, L, N, P, R, T, V, X, and Z are even,
+   * corresponding to the numbers 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, and 26.
+   *
+   * @param s String containing the data to be parsed for &quot;even&quot; letters
+   * @return  String containing the &quot;even&quot; letters from the input
+   */
+   public static String evensOnly(String s ) {
+      String str = s.toUpperCase();
+      String evens = "";
+      for (int i = 0; i <= str.length() - 1; i++) {
+         char temp = str.charAt(i);
+         if (temp == 'B' || temp == 'D' || temp == 'F' || temp == 'H' || temp == 'J'|| temp == 'L' || temp == 'N' || temp == 'P' || temp == 'R' || temp == 'T'|| temp == 'V' || temp == 'X' || temp == 'Z') {
+            evens = evens + temp;
+         }
+      }
+      return evens;
+   }
+
+
+  /**
+   * Method to return the characters in a string that correspond to the &quot;ODD&quot; index
+   * numbers of the alphabet.  The letters A, C, E, G, I, K, M, O, Q, S, U, W, and Y are odd,
+   * corresponding to the numbers 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, and 25.
+   *
+   * @param s String containing the data to be parsed for &quot;odd&quot; letters
+   * @return  String containing the &quot;odd&quot; letters from the input
+   */
+   public static String evensOnly(String s ) {
+      String str = s.toUpperCase();
+      String odds = "";
+      for (int i = 0; i <= str.length() - 1; i++) {
+         char temp = str.charAt(i);
+         if (temp == 'A' || temp == 'C' || temp == 'E' || temp == 'G' || temp == 'I'|| temp == 'K' || temp == 'M' || temp == 'O' || temp == 'Q' || temp == 'S'|| temp == 'U' || temp == 'W' || temp == 'Y') {
+            odds = odds + temp;
+         }
+      }
+      return odds;
+   }
+
+  /**
+   * Method to return the characters in a string that correspond to the &quot;EVEN&quot; index
+   * numbers of the alphabet, but with no duplicate characters in the resulting string.
+   *
+   * @param s String containing the data to be parsed for &quot;even&quot; letters
+   * @return  String containing the &quot;even&quot; letters from the input without duplicates
+   */
+   public static String evensOnlyNoDupes( String s ) {
+      String evens = "";
+      for (int i = 0; i <= s.length() - 1; i += 2) {
+         evens = evens + s.charAt(i);
+      }
+      return evens;
+   }
+
+  /**
+   * Method to return the characters in a string that correspond to the &quot;ODD&quot; index
+   * numbers of the alphabet, but with no duplicate characters in the resulting string.
+   *
+   * @param s String containing the data to be parsed for &quot;odd&quot; letters
+   * @return  String containing the &quot;odd&quot; letters from the input without duplicates
+   */
+   public static String oddsOnlyNoDupes( String s ) {
+      String odds = "";
+      for (int i = 1; i <= s.length() - 1; i += 2) {
+         odds = odds + s.charAt(i);
+      }
+      return odds;
+   }
+
+  /**
+   * Method to return the reverse of a string passed as an argument
+   *
+   * @param s String containing the data to be reversed
+   * @return  String containing the reverse of the input string
+   */
+   public static String reverse( String s ) {
+      String rev = "";
+      for (int i = s.length() - 1; i >= 0; i-- ) {
+         rev = rev + s.charAt (i);
+      }
+      return rev;
+   }
+
+  /**
+   * Main method to test the methods in this class
+   *
+   * @param args String array containing command line parameters
+   */
+   public static void main( String args[] ) {
+      String blah = new String( "Blah blah blah" );
+      String woof = new String( "BCDBCDBCDBCDBCD" );
+      String pal1 = new String( "a" );
+      String pal2 = new String( "ab" );
+      String pal3 = new String( "aba" );
+      String pal4 = new String( "amanaplanacanalpanama" );
+      String pal5 = new String( "abba" );
+      System.out.println( containsVowel( blah ) );
+      System.out.println( containsVowel( woof ) );
+      System.out.println( isPalindrome( pal1 ) );
+      System.out.println( isPalindrome( pal2 ) );
+      System.out.println( isPalindrome( pal3 ) );
+      System.out.println( isPalindrome( pal4 ) );
+      System.out.println( isPalindrome( pal5 ) );
+      System.out.println( "evensOnly()        returns: " + evensOnly( "REHEARSALSZ" ) );
+      System.out.println( "evensOnly()        returns: " + evensOnly( "REhearSALsz" ) );
+      System.out.println( "evensOnlyNoDupes() returns: " + evensOnlyNoDupes( "REhearSALsz" ) );
+      System.out.println( "oddsOnly()         returns: " + oddsOnly( "xylophones" ) );
+      System.out.println( "oddsOnly()         returns: " + oddsOnly( "XYloPHonES" ) );
+      System.out.println( "oddsOnlyNoDupes()  returns: " + oddsOnlyNoDupes( "XYloPHonES" ) );
+      System.out.println( "reverse()          returns: " + reverse( "REHEARSALSZ" ) );
+   }
+}
